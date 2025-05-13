59d0cf4d81c209bbbfbe0b51724d3381fbb8e8ee
ali99944 <alitarek99944@gmail.com>
2025-05-13 22:36:58 +0300

    update

--- src/constants/app_constants.ts
+++ src/constants/app_constants.ts
@@ -1,5 +1,5 @@
 // export const apiUrl = 'http://localhost:5000/api'
-// export const storageUrl = 'http://localhost:5000/storage'
+// export const storageUrl = 'http://localhost:5000/public'
 
 export const apiUrl = 'https://server.oorbis.top/api'
-export const storageUrl = 'https://server.oorbis.top/storage'
+export const storageUrl = 'https://server.oorbis.top/public'
